919bb066501dc4d928f1f7070725360237ca4ba4
officesmm <office.soemyatmin@gmail.com>
2023-07-12 18:10:42 +0900

    "dingtalk-robot-sender": "^1.2.0", update

---
+++ testuploaddingtalk.js
@@ -0,0 +1,5 @@
+const ChatBot = require('dingtalk-robot-sender');
+const robot = new ChatBot({
+   webhook: 'https://oapi.dingtalk.com/robot/send?access_token=290fc32bb41a5326ad0e44fa59ee2f7c60b58f40820a904268665656ab025f73'
+});
+robot.text("nodetest all ok ");
